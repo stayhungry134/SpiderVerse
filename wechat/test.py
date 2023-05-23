5b26b01d6bdef9236f63ceb8a8e9a28fb90f608e
Ethan <stayhungry134@gmail.com>
2023-05-23 17:44:00 +0800

    爬取微信相关的东西

---
+++ wechat/test.py
@@ -0,0 +1,15 @@
+"""
+name: get_shanbay_cookie_win.py
+create_time: 2023-03-03
+author: Ethan White
+
+Description: 
+"""
+
+# 导入爬虫相关的库
+import requests
+
+# 导入解析库
+from lxml import etree
+
+# 导入 json 库
